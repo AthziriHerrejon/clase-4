810a7e966a31129ceaf68b300b3a8594ac9b8f93
AthziriHerrejon <54967484+AthziriHerrejon@users.noreply.github.com>
2019-10-22 14:19:48 -0500

    repo

---
+++ Problema Videojuego.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="application/javascript">
+
+console.log ("¿Cual es tu nivel en este videojuego?")
+ const Aprendiz=9
+ const Acrobata=10
+ const Ninja=20
+ const Nivel=
+
+if  ( Nivel <) {
+    console.log ("Eres Un Aprendiz")
+} else { (Nivel >=20) {
+    console.log ("Eres un Ninja")
+} if {
+    console.log ("Eres un Acrobata")
+    
+    </script>
+    </head>
+    <body>
+    </body>
+    </html>
+
+    PROBLEMA 2
+LEER NivelEnVideojuego
+ Aprendiz=9
+ Acrobata=10
+ Ninja=20
+Nivel= ??
+
+SI Nivel es <10  ENTONCES escribe "Eres Un Aprendiz"
+SINO
+SI Nivel es >=20 ENTONCES escribe "Eres Un Ninja"
+SINO  escribe "Eres Un Acrobata"
